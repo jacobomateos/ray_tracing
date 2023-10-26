97d03332c8b9cb94cf2fdd0acdfb917e65194b3f
Jacobo <jacobobmateos@gmail.com>
2023-10-26 18:29:00 +0200

    implement normal determination

--- sphere.h
+++ sphere.h
@@ -27,7 +27,8 @@ class sphere : public hittable {
 
         rec.t = root;
         rec.p = r.at(rec.t);
-        rec.normal = (rec.p - center) / radius;
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
 
         return true;
     }
