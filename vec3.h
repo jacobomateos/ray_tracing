f17810cd13c3c4b27bd802d4f150870aca36c51a
Jacobo <jacobobmateos@gmail.com>
2023-10-31 20:58:00 +0100

    Added ray diffusion on surfaces.

--- vec3.h
+++ vec3.h
@@ -50,6 +50,13 @@ class vec3 {
         double length() const { return sqrt(length_squared()); }
 
         double length_squared() const { return e[0]* e[0] + e[1]* e[1] + e[2]* e[2]; }
+
+        static vec3 random() {
+            return vec3(random_double(), random_double(), random_double());
+        }
+        static vec3 random(double min, double max) {
+            return vec3(random_double(min, max), random_double(min,max), random_double(min, max));
+        }
 };
 
 // Clarification: using point3 as alias for vec3;
@@ -85,5 +92,25 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 }
 
 inline vec3 unit_vector(vec3 v) { return v / v.length(); }
+
+inline vec3 random_in_unit_sphere() {
+    // Get a random vector on the sphere
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() < 1) return p;
+    }
+}
+
+inline vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
+}
+
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return on_unit_sphere;
+    else
+        return -on_unit_sphere;
+}
 
 #endif 
