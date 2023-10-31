b7f738bf7d5b5073117172d1bfff5d319c3d0800
Jacobo <jacobobmateos@gmail.com>
2023-10-31 07:19:00 +0100

    fixed color rendering typo

--- color.h
+++ color.h
@@ -21,7 +21,7 @@ void write_color (std::ostream &out, color pixel_color, int samples_per_pixel) {
     static const interval intensity(0.000, 0.999);
     out <<static_cast<int>(256 * intensity.clamp(r)) << ' '
         <<static_cast<int>(256 * intensity.clamp(g)) << ' '
-        <<static_cast<int>(256 * intensity.clamp(r)) << '\n';
+        <<static_cast<int>(256 * intensity.clamp(b)) << '\n';
 }
 
 #endif
