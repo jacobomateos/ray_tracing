f552de3f317ff594bfcb77f2b6029f02e726b381
Jacobo <jacobobmateos@gmail.com>
2023-11-14 07:20:54 +0100

    Given condition to dielectric material to only refract if possible

--- main.cc
+++ main.cc
@@ -21,7 +21,7 @@ int main() {
     auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<cuboid>(point3(0.0, 1.0, -1.0), 0.5, 0.9, 0.25, material_center));
+    world.add(make_shared<cuboid>(point3(0.0, 0.0, -1.0), 0.6, 0.6, 0.6, material_center));
     world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
     world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
